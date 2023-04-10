ae5187d6c8a98479c385052fc8e9d022a911f057
Uchenna Ofodile <uchenna.ofodile@gmail.com>
2023-04-10 10:52:18 -0400

    Change to main.tf

--- google-cloud/terraform/main.tf
+++ google-cloud/terraform/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     google ={
         source = "hashicorp/google"
-        version = "~4.37.0" //this version or greater
+        version = "~>4.37.0" //this version or greater
     }
   }
 }
